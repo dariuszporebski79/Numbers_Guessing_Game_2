2e52d7b214ad0e14024fd56dcabbaf3ff792dbbe
dariuszporebski79 <dariusz.porebski79@wp.pl>
2023-04-02 12:20:14 +0200

    ok version

--- Numbers_Guessing_Game_2.py
+++ Numbers_Guessing_Game_2.py
@@ -2,13 +2,19 @@ print("Pomyśl liczbę od 0 do 1000, a ja ja zgadnę w maksymalnie 10 próbach")
 min = 0
 max = 1000
 answer = 0
-while answer != 3:
+while answer != "zgadłeś":
     guess = int((max-min) / 2) + min
-    print("Zgaduję: ", guess)
-    answer = int(input("Za dużo - wpisz 1, za mało - wpisz 2, zgadłem - wpisz 3 "))
-    if answer == 3:
-        print("Wygrałem :-)")
-    elif answer == 1:
-        max = guess
-    else:
-        min = guess
+    print("Zgaduję :-) ;-): ", guess)
+    while True:
+        answer = input("Wpisz \"za dużo\" lub \"za mało\" lub \"zgadłeś\" :-) ")
+        if answer == "zgadłeś":
+            print("Wygrałem :-)")
+            break
+        elif answer == "za dużo":
+            max = guess
+            break
+        elif answer == "za mało":
+            min = guess
+            break
+        else:
+            print("Pomyliłeś się lub oszukujesz ;-)")
